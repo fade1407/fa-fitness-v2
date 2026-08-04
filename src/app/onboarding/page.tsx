6b6514388ff4eca74f5419c5c9520c071125e789
fade1407 <fade1407@gmail.com>
2026-08-04 13:22:56 +0300

    Create page.tsx

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,3 @@
+import { OnboardingFlow } from "@/components/auth/onboarding-flow";
+export const metadata = { title: "إعداد الحساب" };
+export default function OnboardingPage() { return <OnboardingFlow />; }
