b987d3f26ce25baa4776d496cce4fe600db33bf0
fade1407 <fade1407@gmail.com>
2026-08-04 13:22:22 +0300

    Create page.tsx

---
+++ src/app/calendar/page.tsx
@@ -0,0 +1,3 @@
+import { CalendarView } from "@/components/calendar/calendar-view";
+export const metadata = { title: "التقويم" };
+export default function CalendarPage() { return <CalendarView />; }
