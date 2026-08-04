b4209eff20edcf9a601b5daa5f2eca8be41cb393
fade1407 <fade1407@gmail.com>
2026-08-04 13:22:29 +0300

    Create page.tsx

---
+++ src/app/exercises/page.tsx
@@ -0,0 +1,3 @@
+import { ExerciseLibrary } from "@/components/workout/exercise-library";
+export const metadata = { title: "مكتبة التمارين" };
+export default function ExercisesPage() { return <ExerciseLibrary />; }
