979eb31cfdb683e31878ebd8173ebb1aec73481b
fade1407 <fade1407@gmail.com>
2026-08-04 13:23:21 +0300

    Create page.tsx

---
+++ src/app/workouts/start/page.tsx
@@ -0,0 +1,5 @@
+import { TrainingStart } from "@/components/workout/training-start";
+
+export const metadata = { title: "بدء التمرين" };
+
+export default function StartWorkoutPage() { return <TrainingStart />; }
